d0dc5f33b6fda83c9222c2f7f238981ac4ad5f04
re5nardo <re5nardo@naver.com>
2022-07-29 16:38:00 +0900

    RoomResponseDto에 룸 상태 값 및 ip, port 추가

--- LobbyServer/src/dtos/room.dto.ts
+++ LobbyServer/src/dtos/room.dto.ts
@@ -7,9 +7,21 @@ export class RoomResponseDto {
     public matchType: MatchType;
     public subGameId: string;
     public mapId: string;
+    public status: RoomStatus;
+    public ip: string;
+    public port: number;
 }
 
 export class GetRoomResponseDto implements ResponseBase {
     public code: number;
     public room?: RoomResponseDto;
+}
+
+export enum RoomStatus {
+    None = 0,
+    Spawning = 1,
+    Spawned = 2,
+    Ready = 3,
+    Playing = 4,
+    Finished = 5,
 }
