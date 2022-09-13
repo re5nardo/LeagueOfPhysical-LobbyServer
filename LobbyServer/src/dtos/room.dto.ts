649dcd5b405acfa60546f253ff73190ef9a9295a
re5nardo <re5nardo@naver.com>
2022-09-13 22:18:30 +0900

    room.interface 추가 및 적용

--- LobbyServer/src/dtos/room.dto.ts
+++ LobbyServer/src/dtos/room.dto.ts
@@ -1,12 +1,9 @@
 import { ResponseBase } from '@interfaces/responseBase.interface';
-import { MatchType } from '@interfaces/match.interface';
+import { RoomStatus } from '@interfaces/room.interface';
 
 export class RoomResponseDto {
     public id: string;
     public matchId: string;
-    public matchType: MatchType;
-    public subGameId: string;
-    public mapId: string;
     public status: RoomStatus;
     public ip: string;
     public port: number;
@@ -15,13 +12,4 @@ export class RoomResponseDto {
 export class GetRoomResponseDto implements ResponseBase {
     public code: number;
     public room?: RoomResponseDto;
-}
-
-export enum RoomStatus {
-    None = 0,
-    Spawning = 1,
-    Spawned = 2,
-    Ready = 3,
-    Playing = 4,
-    Finished = 5,
 }
