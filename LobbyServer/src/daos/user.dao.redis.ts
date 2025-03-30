14d6290379525b67bd711ef240b0c617d5727b28
re5nardo <re5nardo@naver.com>
2025-03-30 21:07:04 +0900

    redis entity 사용 누락 수정

--- LobbyServer/src/daos/user.dao.redis.ts
+++ LobbyServer/src/daos/user.dao.redis.ts
@@ -1,10 +1,10 @@
-import { User } from "@interfaces/user.interface";
+import { User as UserEntity } from '@prisma/client';
 import { DaoRedisBase } from '@daos/dao.redis.base';
 
 const TTL: number = 5 * 60;  //  sec
 const USER_PREFIX: string = 'USER_PREFIX';
 
-export class UserDaoRedis extends DaoRedisBase<User> {
+export class UserDaoRedis extends DaoRedisBase<UserEntity> {
 
     get Prefix() : string {
         return USER_PREFIX;
